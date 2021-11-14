8935a24004dbfbefe9dfa72cd0c2898f06a195f0
Al-Kafw <81038871+Al-Kafw@users.noreply.github.com>
2021-11-14 22:10:31 +0300

    Delete output.tf

--- output.tf
+++
@@ -1,8 +0,0 @@
-output "s3" {
-  value       = module.s3
-  description = "Output values of s3 module"
-}
-output "cloudtrail" {
-  value       = module.cloudtrail
-  description = "output values of cloudtrail"
-}
